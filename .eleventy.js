74747552e81965bb8f390d5a82ff344b70f93684
Aaron J Yoder <aaronjyoder@gmail.com>
2023-11-15 01:26:06 -0500

    Add favicons

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
 module.exports = config => {
+	
+  // Copy `src/images/favicons/` to `build/`
+  config.addPassthroughCopy({ "src/images/icons/favicons": "/" });
+	
   return {
     dir: {
       input: 'src',
