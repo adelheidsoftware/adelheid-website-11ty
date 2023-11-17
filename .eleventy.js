bdd43aab0b6c8a622220faeeb6c256aa4f845c25
Aaron J Yoder <aaronjyoder@gmail.com>
2023-11-17 07:06:36 -0500

    Add RSS plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,15 @@
+const rssPlugin = require('@11ty/eleventy-plugin-rss');
+
 // Filters
 const dateFilter = require('./src/filters/date-filter.js');
 const w3DateFilter = require('./src/filters/w3-date-filter.js');
 const dateTimeFilter = require('./src/filters/datetime-filter.js');
 
 module.exports = config => {
+
+  // Plugins
+  config.addPlugin(rssPlugin);
+
   config.addFilter('dateFilter', dateFilter);
   config.addFilter('w3DateFilter', w3DateFilter);
   config.addFilter('dateTimeFilter', dateTimeFilter);
