2ed36be8791840a463fa42d823ed922e005447c2
Aaron J Yoder <aaronjyoder@gmail.com>
2023-11-18 13:23:47 -0500

    Update gulpfile.mjs

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -3,6 +3,7 @@ const { parallel, watch: gulpWatch } = gulp;
 
 // Pull in each task
 import fonts from "./gulp-tasks/fonts.mjs";
+import images from "./gulp-tasks/images.mjs";
 import sass from "./gulp-tasks/sass.mjs";
 
 // Set each directory and contents that we want to watch and
@@ -10,11 +11,12 @@ import sass from "./gulp-tasks/sass.mjs";
 // prevent the task being run when we run `gulp watch`, but it
 // will run when a file changes.
 const watcher = () => {
+  gulpWatch("./src/images/**/*", { ignoreInitial: true }, images);
   gulpWatch("./src/scss/**/*.scss", { ignoreInitial: true }, sass);
 };
 
 // The default (if someone just runs `gulp`) is to run each task in parallel
-export default parallel(fonts, sass);
+export default parallel(fonts, images, sass);
 
 // This is our watcher task that instructs gulp to watch directories and
 // act accordingly
